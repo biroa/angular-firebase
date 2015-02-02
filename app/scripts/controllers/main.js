67805d84b325fc56ffe4b93020e3320b00d3f657
biroa <adam.biro@gmail.com>
2015-02-02 23:23:37 +0000

    default firebase crus.

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -9,9 +9,25 @@
  */
 angular.module('publicHtmlApp')
   .controller('MainCtrl', function ($scope) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
+	var rootRef = new Firebase('https://glaring-torch-7103.firebaseio.com/messages');
+	var childRef = rootRef.child('message');
+
+	$scope.setMessage = function(){
+	    rootRef.child('message').set({
+		user: 'Adam',
+		text: 'Hi'
+	    })
+	}
+`	//update can only update property
+	// set always overwrite the existing object
+	$scope.updateMessage = function(){
+	    childRef.update({
+		text:'Bye'
+	    })
+	}
+
+	$scope.deleteMessage = function(){
+	    childRef.remove()
+	}
+
   });
