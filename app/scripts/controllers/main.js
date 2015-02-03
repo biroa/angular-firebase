956d7d21ff87ae4cc8c4008753d09c00a820d4c2
biroa <adam.biro@gmail.com>
2015-02-03 21:45:46 +0000

    realtime firebase db mod

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -8,9 +8,28 @@
  * Controller of the publicHtmlApp
  */
 angular.module('publicHtmlApp')
-  .controller('MainCtrl', function ($scope) {
+  .controller('MainCtrl', function ($scope,$timeout) {
 	var rootRef = new Firebase('https://glaring-torch-7103.firebaseio.com/messages');
 	var childRef = rootRef.child('message');
+	
+	$scope.$watch('message.text', function(newVal){
+	    //Make Sure it is not undefined
+	    if(!newVal){
+		return;
+	    }
+	    childRef.update({
+		text:newVal
+	    });
+	});
+
+	//With on we check firebase on realtime
+	childRef.on('value',function(snapshot){
+	    $timeout(function(){
+		var snapshotVal = snapshot.val();
+		console.log(snapshotVal);
+		$scope.message = snapshot.val();
+	    });
+	});
 
 	$scope.setMessage = function(){
 	    rootRef.child('message').set({
