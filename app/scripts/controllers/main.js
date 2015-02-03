1810a9346afa4b8b3fe6f31f0cb38b3749efce2a
biroa <adam.biro@gmail.com>
2015-02-03 22:47:03 +0000

    save a new node

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -12,11 +12,26 @@ angular.module('publicHtmlApp')
 	var rootRef = new Firebase('https://glaring-torch-7103.firebaseio.com/messages');
 	var messagesRef = rootRef.child('messages');
 	
-	childRef.on('value',function(snapshot){
+	$scope.currentUser = null;
+	$scope.currentText = null;
+
+	messagesRef.on('value',function(snapshot){
 	    $timeout(function(){
 		var snapshotVal = snapshot.val();
-		$scope.message = snapshot.val();
+		$scope.messages = snapshot.val();
 	    });
 	});
+
+	$scope.sendMessage = function(){
+	    var newMessage = {
+		user: $scope.currentUser,
+		text: $scope.currentText
+	    };
+	    //set allows to control the name
+	    //push create a unique name
+	    messagesRef.push(newMessage);
+	    
+	}
+	
 
   });
