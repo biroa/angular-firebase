3eaca0c7da4b5d2490d72c68a11049cb9d8537d0
biroa <adam.biro@gmail.com>
2015-02-05 22:05:57 +0000

    turn off Firebase stream

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -8,29 +8,36 @@
  * Controller of the publicHtmlApp
  */
 angular.module('publicHtmlApp')
-  .controller('MainCtrl', function ($scope,$timeout) {
-	var rootRef = new Firebase('https://glaring-torch-7103.firebaseio.com/messages');
-	var messagesRef = rootRef.child('messages');
-	
-	$scope.currentUser = null;
-	$scope.currentText = null;
-	$scope.messages = [];
+	  .controller('MainCtrl', function ($scope,$timeout) {
+		var rootRef = new Firebase('https://glaring-torch-7103.firebaseio.com/messages');
+		var messagesRef = rootRef.child('messages');
 
-	//value -> all items
-	//child_added -> on first load get all data after that get only the last item when add someone a new item
-	//with new child added we save exponentially increasing load that would cause value
-	// with
-	messagesRef.on('child_added',function(snapshot){
-	    $timeout(function(){
-		var snapshotVal = snapshot.val();
-		$scope.messages.push({
-			text: snapshotVal.text,
-			user: snapshotVal.user,
-			key: snapshot.key()
-			}
-			);
-	    });
-	});
+		$scope.currentUser = null;
+		$scope.currentText = null;
+		$scope.messages = [];
+
+		//value -> all items
+		//child_added -> on first load get all data after that get only the last item when add someone a new item
+		//with new child added we save exponentially increasing load that would cause value
+		// with
+		messagesRef.on('child_added',function(snapshot){
+			$timeout(function(){
+			var snapshotVal = snapshot.val();
+			$scope.messages.push({
+				text: snapshotVal.text,
+				user: snapshotVal.user,
+				key: snapshot.key()
+				}
+				);
+			});
+		});
+
+		$scope.turnFeedOff = function(){
+			//we do not get any data
+			console.log('turned off');
+			messagesRef.off;
+		}
+
 
 		messagesRef.on('child_changed', function(snapshot) {
 			$timeout(function() {
@@ -78,4 +85,9 @@ angular.module('publicHtmlApp')
 			messagesRef.push(newMessage);
 		};
 
+		$scope.turnFeedOff = function(){
+			//we do not get any data
+			console.log('turned off');
+			messagesRef.off;
+		}
 	});
