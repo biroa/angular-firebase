08e2f9e98a3615e30db6859fc860c1907964edb4
biroa <adam.biro@gmail.com>
2015-02-06 22:44:28 +0000

    limitToFirst(), limitToLast(), startAt(), endAt() functions usage

--- app/scripts/services/MessageService.js
+++ app/scripts/services/MessageService.js
@@ -6,7 +6,11 @@
         var messageRef = new Firebase(FBURL).child('messages');
         return {
             childAdded: function childAdded(limitNumber,cb) {
-                messageRef.limit(limitNumber).on('child_added', function(snapshot) {
+                //limit retrieve the last x items: limitToFirst(x)
+                //limit retrieve the last x items: limitToLast(x)
+                //startAt() modifies it to the first x = key(): startAt(priority,key())
+                //endAt()
+                messageRef.startAt(null,'-JhLhFJgVT1756I7BrbC').endAt(null,'-JhLhFJgVT1756I7BrbC').on('child_added', function(snapshot) {
                     var val = snapshot.val();
                     cb.call(this, {
                         user: val.user,
